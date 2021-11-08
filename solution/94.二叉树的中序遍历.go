2cddb4b3414bf1aefa7a65bb3d066bf4784ac5f1
gleemanx <gleemanx@126.com>
2021-11-08 12:05:59 +0800

    二叉树的中序遍历

---
+++ solution/94.二叉树的中序遍历.go
@@ -0,0 +1,41 @@
+package solution
+
+/*
+ * @lc app=leetcode.cn id=94 lang=golang
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func inorderTraversal(root *TreeNode) []int {
+	res := []int{}
+	if root == nil {
+		return res
+	}
+	LDR(&res, root)
+	return res
+}
+
+func LDR(n *[]int, root *TreeNode) {
+	if root != nil {
+		LDR(n, root.Left)
+		*n = append(*n, root.Val)
+		LDR(n, root.Right)
+	}
+}
+
+// @lc code=end
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
