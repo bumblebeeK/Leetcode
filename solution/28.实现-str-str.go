d244bb9a5cae40cd9fcfe2a022a799454c0c9636
gleemanx <gleemanx@126.com>
2021-11-10 10:03:17 +0800

    删除不必要的实现

--- solution/28.实现-str-str.go
+++ solution/28.实现-str-str.go
@@ -69,27 +69,6 @@ func getNext(needle string) []int {
 	}
 	return next
 }
-func KMP3(p string, s string) int {
-
-	i, j := 0, 0
-	pLen := len(p)
-	sLen := len(s)
-	next := getNext3(p)
-	for i < sLen && j < pLen {
-		if j == -1 || s[i] == p[j] { //s[i]!=s[0]=>j=next[0]=-1,第0位不匹配所以i++，j++;j=0
-			i++
-			j++
-		} else {
-			j = next[j]
-		}
-	}
-	if j == pLen {
-		return i - j
-	} else {
-		return -1
-	}
-
-}
 func getNext2(needle string) []int {
 	next := make([]int, len(needle)+1, len(needle)+1)
 	next[0] = -1
@@ -106,32 +85,6 @@ func getNext2(needle string) []int {
 
 	}
 	return next
-}
-
-func getNext3(p string) []int {
-	pLen := len(p)
-	next := make([]int, pLen+1, pLen+1)
-	next[0] = -1
-	next[1] = 0
-	i := 0
-	j := 1
-	for j < pLen-1 { //因为next[pLen-1]由s[i] == s[pLen-2]算出
-		if i == -1 || p[i] == p[j] { //-1代表了起始位不匹配，i=0,s[0]!=s[j]=>i=next[0]=-1
-			i++
-			j++
-			if p[i] != p[j] { //因为出现在j位置不匹配的话会跳到next[j]=i位置去匹配,p[i] == p[j]肯定又是不匹配（优化核心点）
-				next[j] = i
-			} else {
-				next[j] = next[i]
-			}
-
-		} else {
-			i = next[i]
-		}
-	}
-
-	return next
-
 }
 
 // @lc code=end
