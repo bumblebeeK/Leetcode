ebc5974c45814ac00c36fe988e124f794cc3f937
gleemanx <gleemanx@126.com>
2021-11-16 16:49:26 +0800

    无重复字符的最长子串

--- solution/3.无重复字符的最长子串.go
+++ solution/3.无重复字符的最长子串.go
@@ -7,8 +7,30 @@ package solution
  */
 
 // @lc code=start
-// func lengthOfLongestSubstring(s string) int {
+func lengthOfLongestSubstring(s string) int {
+	if s == "" || len(s) == 0 {
+		return 0
+	}
+	var m = make(map[byte]int)
+	cur, prev := 0, 0
+	for i := 0; i < len(s); i++ {
+		if index, ok := m[s[i]]; ok && index >= i-cur {
+			prev = max(prev, cur)
+			cur = i - index
 
-// }
+		} else {
+			cur++
+		}
+		m[s[i]] = i
+	}
+	return max(cur, prev)
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
 
 // @lc code=end
