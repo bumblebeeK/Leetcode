e55646437b769645ab5505fe2049f1e18a25cb65
gleemanx <gleemanx@126.com>
2021-11-24 11:31:38 +0800

    加一

---
+++ solution/66.加一.go
@@ -0,0 +1,23 @@
+package solution
+
+/*
+ * @lc app=leetcode.cn id=66 lang=golang
+ *
+ * [66] 加一
+ */
+
+// @lc code=start
+func plusOne(digits []int) []int {
+	for i := len(digits) - 1; i >= 0; i-- {
+		digits[i]++
+		digits[i] = digits[i] % 10
+		if digits[i] != 0 {
+			return digits
+		}
+	}
+	digits = make([]int, len(digits)+1)
+	digits[0] = 1
+	return digits
+}
+
+// @lc code=end
