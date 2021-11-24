13f060d59473aaa10ddcfb2d8b02bc5c1cbfd537
gleemanx <gleemanx@126.com>
2021-11-24 14:56:06 +0800

    最接近的三数之和

---
+++ solution/16.最接近的三数之和.go
@@ -0,0 +1,44 @@
+package solution
+
+import (
+	"sort"
+)
+
+/*
+ * @lc app=leetcode.cn id=16 lang=golang
+ *
+ * [16] 最接近的三数之和
+ */
+
+// @lc code=start
+func threeSumClosest(nums []int, target int) int {
+	sort.Ints(nums)
+	ans := nums[0] + nums[1] + nums[2]
+	for i := 0; i < len(nums); i++ {
+		start, end := i+1, len(nums)-1
+		for end > start {
+			sum := nums[i] + nums[start] + nums[end]
+			if sampleAbs(sum-target) < sampleAbs(ans-target) {
+				ans = sum
+			}
+			if sum > target {
+				end--
+			} else if sum < target {
+				start++
+			} else {
+				return target
+			}
+		}
+	}
+	return ans
+}
+
+func sampleAbs(num int) int {
+	if num > 0 {
+		return num
+	} else {
+		return -num
+	}
+}
+
+// @lc code=end
