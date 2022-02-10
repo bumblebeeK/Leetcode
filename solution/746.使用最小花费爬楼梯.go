a705695cd49973df56d10b507ac79057d10e5edd
gleemanx <gleemanx@126.com>
2022-02-10 13:57:29 +0800

    Create 746.使用最小花费爬楼梯.go

---
+++ solution/746.使用最小花费爬楼梯.go
@@ -0,0 +1,14 @@
+package solution
+
+/*
+ * @lc app=leetcode.cn id=746 lang=golang
+ *
+ * [746] 使用最小花费爬楼梯
+ */
+
+// @lc code=start
+func minCostClimbingStairs(cost []int) int {
+
+}
+// @lc code=end
+
