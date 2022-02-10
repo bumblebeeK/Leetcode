ad317e2a46ee09c1b7ef3c8f723af8a535d148c3
gleemanx <gleemanx@126.com>
2022-02-10 10:24:08 +0800

    Update 198.打家劫舍.go
    2.10复习

--- solution/198.打家劫舍.go
+++ solution/198.打家劫舍.go
@@ -8,14 +8,22 @@ package solution
 
 // @lc code=start
 // func rob(nums []int) int {
-func rob1(nums []int) int {
-	curv, prev := 0, 0
+func rob(nums []int) int {
+	cur, pre := 0, 0
 	for _, v := range nums {
-		temp := curv
-		curv = Max(temp, prev+v)
-		prev = temp
+		tmp := cur
+		cur = Max(pre+v, cur)
+		pre = tmp
 	}
-	return curv
+	return cur
+}
+
+func Max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
 }
 
 // @lc code=end
