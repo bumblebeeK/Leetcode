f214532fe2556a2bf2dc874a5a999f8ffc7b583d
gleemanx <gleemanx@126.com>
2022-02-10 11:16:05 +0800

    Update 75.颜色分类.go

--- solution/75.颜色分类.go
+++ solution/75.颜色分类.go
@@ -7,17 +7,36 @@ package solution
  */
 
 // @lc code=start
-func sortColors(nums []int) {
-	m := map[int]int{}
-	for _, num := range nums {
-		m[num]++
-	}
 
-	for i, j := 0, 0; i < 3; i++ {
-		for m[i] != 0 {
-			m[i]--
-			nums[j] = i
-			j++
+// Hash方法
+
+// func sortColors(nums []int) {
+// 	m := map[int]int{}
+// 	for _, num := range nums {
+// 		m[num]++
+// 	}
+
+// 	for i, j := 0, 0; i < 3; i++ {
+// 		for m[i] != 0 {
+// 			m[i]--
+// 			nums[j] = i
+// 			j++
+// 		}
+// 	}
+// }
+
+// 计数抢占法
+func sortColors(nums []int) {
+	zero, one := 0, 0
+	for i, num := range nums {
+		nums[i] = 2
+		if num < 2 {
+			nums[one] = 1
+			one++
+		}
+		if num < 1 {
+			nums[zero] = 0
+			zero++
 		}
 	}
 }
