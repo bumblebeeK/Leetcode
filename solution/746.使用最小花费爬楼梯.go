c4c18b7ec96a24845a189368699d64f4f19f122d
gleemanx <gleemanx@126.com>
2022-02-10 14:19:01 +0800

    Update 746.使用最小花费爬楼梯.go

--- solution/746.使用最小花费爬楼梯.go
+++ solution/746.使用最小花费爬楼梯.go
@@ -8,7 +8,19 @@ package solution
 
 // @lc code=start
 func minCostClimbingStairs(cost []int) int {
-
+	dp := make([]int, len(cost))
+	dp[0], dp[1] = cost[0], cost[1]
+	for i := 2; i < len(cost); i++ {
+		dp[i] = cost[i] + minCost(dp[i-1], dp[i-2])
+	}
+	return minCost(dp[len(cost)-1], dp[len(cost)-2])
 }
-// @lc code=end
 
+func minCost(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+// @lc code=end
