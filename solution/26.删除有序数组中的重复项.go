e361a62806d1499f4fb5187828c7865e349710a5
gleemanx <gleemanx@126.com>
2022-02-11 13:42:39 +0800

    Create 26.删除有序数组中的重复项.go

---
+++ solution/26.删除有序数组中的重复项.go
@@ -0,0 +1,26 @@
+package solution
+
+/*
+ * @lc app=leetcode.cn id=26 lang=golang
+ *
+ * [26] 删除有序数组中的重复项
+ */
+
+// @lc code=start
+func removeDuplicates(nums []int) int {
+	l := len(nums)
+	if l < 1 {
+		return l
+	}
+	fast, slow := 1, 0
+	for fast < l {
+		if nums[fast] != nums[slow] {
+			slow++
+			nums[slow] = nums[fast]
+		}
+		fast++
+	}
+	return slow + 1
+}
+
+// @lc code=end
