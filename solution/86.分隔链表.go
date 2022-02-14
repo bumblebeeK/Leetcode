6d655fc61bb47c87340a02fd48fb4d6072d391bc
gleemanx <gleemanx@126.com>
2022-02-14 17:03:38 +0800

    Update 86.分隔链表.go

--- solution/86.分隔链表.go
+++ solution/86.分隔链表.go
@@ -14,36 +14,64 @@ package solution
  *     Next *ListNode
  * }
  */
+// func partition(head *ListNode, x int) *ListNode {
+// 	if head == nil {
+// 		return head
+// 	}
+// 	newHead := &ListNode{0, head}
+// 	mark := newHead
+// 	for mark != nil {
+// 		if mark.Next != nil && mark.Next.Val >= x {
+// 			break
+// 		}
+// 		mark = mark.Next
+// 	}
+// 	if mark == nil {
+// 		return head
+// 	}
+// 	after, last := mark.Next, mark.Next
+// 	for after != nil {
+// 		if after.Val < x {
+// 			next := after.Next
+// 			target := mark.Next
+// 			mark.Next = after
+// 			after.Next = target
+// 			mark = mark.Next
+// 			after = next
+// 			last.Next = after
+// 		} else {
+// 			last = after
+// 			after = after.Next
+// 		}
+// 	}
+
+// 	return newHead.Next
+
+// }
+
 func partition(head *ListNode, x int) *ListNode {
 	if head == nil {
 		return head
 	}
-	newHead := &ListNode{0, head}
-	mark := newHead
-	for mark != nil {
-		if mark.Next != nil && mark.Next.Val >= x {
-			break
-		}
-		mark = mark.Next
-	}
-	if mark == nil {
+	ln := partition(head.Next, x)
+	if head.Val < x {
+		head.Next = ln
 		return head
 	}
-	after, last := mark.Next, mark.Next
-	for after != nil {
-		if after.Val < x {
-			next := after.Next
-			target := mark.Next
-			mark.Next = after
-			after.Next = target
-			mark = mark.Next
-			after = next
-			last.Next = after
-		} else {
-			last = after
-			after = after.Next
+	prev := &ListNode{}
+	prev.Next = ln
+	newHead := prev
+	for ln != nil {
+		if ln.Val >= x {
+			prev.Next = head
+			head.Next = ln
+			return newHead.Next
 		}
+		prev = ln
+		ln = ln.Next
 	}
+	prev.Next = head
+	head.Next = nil
 
 	return newHead.Next
 
