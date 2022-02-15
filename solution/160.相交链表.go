cb5f1274eb35e06ef148e0410baa9bb6da195a0d
gleemanx <gleemanx@126.com>
2022-02-15 12:42:14 +0800

    Create 160.相交链表.go

---
+++ solution/160.相交链表.go
@@ -0,0 +1,38 @@
+package solution
+
+/*
+ * @lc app=leetcode.cn id=160 lang=golang
+ *
+ * [160] 相交链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	A, B := headA, headB
+	for A != B {
+		if A != nil {
+			A = A.Next
+		} else {
+			A = headB
+		}
+		if B != nil {
+			B = B.Next
+		} else {
+			B = headA
+		}
+	}
+	return A
+}
+
+// @lc code=end
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
