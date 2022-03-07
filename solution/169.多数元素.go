987237d94e588714ca122ac2fea3758848df01a5
gleemanx <gleemanx@126.com>
2022-03-07 22:42:15 +0800

    Create 169.多数元素.go

---
+++ solution/169.多数元素.go
@@ -0,0 +1,17 @@
+package solution
+
+import "sort"
+
+/*
+ * @lc app=leetcode.cn id=169 lang=golang
+ *
+ * [169] 多数元素
+ */
+
+// @lc code=start
+func majorityElement(nums []int) int {
+	sort.Ints(nums)
+	return nums[len(nums)/2]
+}
+
+// @lc code=end
