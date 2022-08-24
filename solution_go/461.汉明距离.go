65c3d4432ad2fe8a7d60ec672d875b2af160e7bf
bumblebeeK <gleemanx@126.com>
2022-08-24 16:37:51 +0800

    Update 461.汉明距离.go

--- solution_go/461.汉明距离.go
+++ solution_go/461.汉明距离.go
@@ -1,9 +1,5 @@
 package solution
 
-import (
-	"math/bits"
-)
-
 /*
  * @lc app=leetcode.cn id=461 lang=golang
  *
@@ -12,7 +8,13 @@ import (
 
 // @lc code=start
 func hammingDistance(x int, y int) int {
-	return bits.OnesCount(uint(x ^ y))
+	diff := x ^ y
+	count := 0
+	for diff > 0 {
+		diff &= diff - 1
+		count++
+	}
+	return count
 }
 
 // @lc code=end
