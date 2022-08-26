107331087aa2b8b6f0005987098f0565c34daaba
bumblebeeK <gleemanx@126.com>
2022-08-26 14:21:46 +0800

    Create 64.最小路径和.go

---
+++ solution_go/64.最小路径和.go
@@ -0,0 +1,34 @@
+package solution
+
+/*
+ * @lc app=leetcode.cn id=64 lang=golang
+ *
+ * [64] 最小路径和
+ */
+
+// @lc code=start
+func minPathSum(grid [][]int) int {
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[0]); j++ {
+			if i == 0 && j == 0 {
+				continue
+			} else if i == 0 {
+				grid[i][j] += grid[i][j-1]
+			} else if j == 0 {
+				grid[i][j] += grid[i-1][0]
+			} else {
+				grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+			}
+		}
+	}
+	return grid[len(grid)-1][len(grid[0])-1]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// @lc code=end
