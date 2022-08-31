0ce5af34ea5895f86e842e4ac68800e140faf324
bumblebeeK <gleemanx@126.com>
2022-08-31 10:14:09 +0800

    Create 31.下一个排列.go

---
+++ solution_go/31.下一个排列.go
@@ -0,0 +1,32 @@
+package solution
+
+/*
+ * @lc app=leetcode.cn id=31 lang=golang
+ *
+ * [31] 下一个排列
+ */
+
+// @lc code=start
+func nextPermutation(nums []int) {
+	n := len(nums)
+	i := n - 2
+	for i >= 0 && nums[i] >= nums[i+1] {
+		i--
+	}
+	if i >= 0 {
+		j := n - 1
+		for j >= 0 && nums[j] <= nums[i] {
+			j--
+		}
+		nums[i], nums[j] = nums[j], nums[i]
+	}
+	reverse(nums[i+1:])
+}
+
+func reverse(nums []int) {
+	for i, n := 0, len(nums); i < n/2; i++ {
+		nums[i], nums[n-i-1] = nums[n-i-1], nums[i]
+	}
+}
+
+// @lc code=end
