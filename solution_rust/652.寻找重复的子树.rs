a4d1aaba2853bb6f435879c086a92d03c10944b8
bumblebeeK <gleemanx@126.com>
2022-09-05 14:49:13 +0800

    Create 652.寻找重复的子树.rs

---
+++ solution_rust/652.寻找重复的子树.rs
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=652 lang=rust
+ *
+ * [652] 寻找重复的子树
+ */
+
+// @lc code=start
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+//
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn find_duplicate_subtrees(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Option<Rc<RefCell<TreeNode>>>> {
+
+    }
+
+}
+// @lc code=end
+
