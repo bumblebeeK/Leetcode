d76c90f0201b1909df66f28b52b77df7c3998e61
bumblebeeK <gleemanx@126.com>
2022-09-05 16:07:14 +0800

    Create 300.最长递增子序列.go

---
+++ solution_go/300.最长递增子序列.go
@@ -0,0 +1,39 @@
+package solution
+
+/*
+ * @lc app=leetcode.cn id=300 lang=golang
+ *
+ * [300] 最长递增子序列
+ */
+
+// @lc code=start
+func lengthOfLIS(nums []int) int {
+	n := len(nums)
+	if n == 0 {
+		return 0
+	}
+	dp := make([]int, n)
+	dp[0] = 1
+	ans := 1
+
+	for i := 1; i < n; i++ {
+		dp[i] = 1
+		for j := 0; j < i; j++ {
+			if nums[i] > nums[j] {
+				dp[i] = max(dp[j]+1, dp[i])
+			}
+		}
+		ans = max(dp[i], ans)
+	}
+	return ans
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
+
+// @lc code=end
