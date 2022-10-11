be337fe851710b1e87b3d2de0ae0f96e2b80e796
bumblebeeK <gleemanx@126.com>
2022-10-11 09:46:47 +0800

    Create 1790.仅执行一次字符串交换能否使两个字符串相等.go

---
+++ solution_go/1790.仅执行一次字符串交换能否使两个字符串相等.go
@@ -0,0 +1,42 @@
+package solution
+
+/*
+ * @lc app=leetcode.cn id=1790 lang=golang
+ *
+ * [1790] 仅执行一次字符串交换能否使两个字符串相等
+ */
+
+// @lc code=start
+func areAlmostEqual(s1 string, s2 string) bool {
+	if s1 == s2 {
+		return true
+	}
+	if len(s1) == 2 {
+		return s1[0] == s2[1] && s1[1] == s2[0]
+	}
+
+	counter := 0
+	var diff0, diff1 byte
+	for i, j := 0, 0; i < len(s1); i, j = i+1, j+1 {
+		if s1[i] == s2[j] {
+			continue
+		} else {
+			if counter == 0 {
+				diff0 = s1[i] - s2[j]
+			}
+			if counter == 1 {
+				diff1 = s1[i] - s2[j]
+			}
+			counter++
+			if counter > 2 {
+				return false
+			}
+		}
+	}
+	if diff0+diff1 == 0 {
+		return true
+	}
+	return false
+}
+
+// @lc code=end
