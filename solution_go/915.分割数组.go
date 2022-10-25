f71da186695f5abf7a3176f66cc18cbed6c3bf25
bumblebeeK <gleemanx@126.com>
2022-10-25 10:49:05 +0800

    Create 915.分割数组.go

---
+++ solution_go/915.分割数组.go
@@ -0,0 +1,42 @@
+package solution
+
+/*
+ * @lc app=leetcode.cn id=915 lang=golang
+ *
+ * [915] 分割数组
+ */
+
+// @lc code=start
+func partitionDisjoint(nums []int) int {
+	n := len(nums)
+	min := make([]int, n)
+	min[n-1] = nums[n-1]
+	for i := n - 2; i >= 0; i-- {
+		min[i] = Min(nums[i], min[i+1])
+	}
+
+	for i, max := 0, 0; i < n-1; i++ {
+		max = Max(nums[i], max)
+		if max <= min[i+1] {
+			return i + 1
+		}
+	}
+
+	return -1
+}
+
+func Min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// @lc code=end
